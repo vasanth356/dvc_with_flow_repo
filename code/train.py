6f61a129572c0ccaf75e6eb9514debd9b6ac4163
vasanth356 <chanukya.vasanth7@gmail.com>
2022-03-01 18:34:35 +0530

    adding the fifth version of the data

--- code/train.py
+++ code/train.py
@@ -25,7 +25,7 @@ import dvc.api
 
 path = r'data\winequality-red.csv'
 repo = r'C:\Users\LENOVO\PycharmProjects\dvc_with_flow_repo'
-version = 'version3'
+version = 'version4'
 
 data_url =  dvc.api.get_url(
     path = path,
